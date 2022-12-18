7e9c28aa03ea0030b122bf8f837091580405cda4
Moro-Afriyie <ceremoinc99@gmail.com>
2022-12-18 20:52:55 +0000

    feat:create the onboarding screens

--- screens/onboarding/Onboarding.tsx
+++ screens/onboarding/Onboarding.tsx
@@ -7,6 +7,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
   useWindowDimensions,
 } from 'react-native';
@@ -46,11 +47,15 @@ const Slide: React.FunctionComponent<{item: ISlide}> = ({item}) => {
   const {width, height} = useWindowDimensions();
 
   return (
-    <View style={slideStyles.container}>
+    <View style={[slideStyles.container, {height: height}]}>
       <Image
         source={item.imageUrl}
         style={[{width: width}, slideStyles.image]}
       />
+      <View style={[slideStyles.footer, {height: height * 0.38, width: width}]}>
+        <Text style={slideStyles.title}>{item.title}</Text>
+        <Text style={slideStyles.subTitle}>{item.subTitle}</Text>
+      </View>
     </View>
   );
 };
@@ -58,26 +63,112 @@ const Slide: React.FunctionComponent<{item: ISlide}> = ({item}) => {
 const slideStyles = StyleSheet.create({
   container: {
     paddingTop: 10,
+    flex: 1,
+    position: 'relative',
+    // backgroundColor: '#ffffff',
   },
-
   image: {
-    height: '75%',
+    height: '62%',
+    // resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '500',
+    lineHeight: 34,
+    textAlign: 'center',
+    marginBottom: 15,
+    color: '#ffffff',
+  },
+  subTitle: {
+    fontSize: 17,
+    fontWeight: '400',
+    lineHeight: 25,
+    textAlign: 'center',
+    color: '#ffffff',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    height: 288,
+    paddingVertical: 40,
+    paddingHorizontal: 50,
+    borderTopLeftRadius: 48,
+    borderTopRightRadius: 48,
+    backgroundColor: '#5669FF',
+  },
+});
+
+const Footer: React.FunctionComponent = () => {
+  const {width} = useWindowDimensions();
+
+  return (
+    <View style={[footerStyles.footerContainer, {width: width}]}>
+      <TouchableOpacity>
+        <Text style={[footerStyles.controls]}>Skip</Text>
+      </TouchableOpacity>
+      <View style={footerStyles.indicatorContainer}>
+        {slides.map(item => (
+          <View key={item.id} style={footerStyles.indicator} />
+        ))}
+      </View>
+
+      <TouchableOpacity>
+        <Text style={[footerStyles.controls]}>Next</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const footerStyles = StyleSheet.create({
+  footerContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 80,
+    paddingHorizontal: 50,
+    paddingVertical: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  indicatorContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  indicator: {
+    width: 10,
+    height: 10,
+    borderRadius: 50,
+    backgroundColor: '#ffffff',
+    opacity: 0.4,
+    marginHorizontal: 4,
+  },
+  controls: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#ffffff',
   },
 });
 
 const OnBoardingScreen: React.FunctionComponent = () => {
-  const {width, height} = useWindowDimensions();
+  const {height} = useWindowDimensions();
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={'#ffffff'} />
-      <FlatList
-        data={slides}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        keyExtractor={item => item.id}
-        renderItem={({item}) => <Slide item={item} />}
-      />
+      <View style={[{height: height}, styles.listContainer]}>
+        <FlatList
+          data={slides}
+          pagingEnabled
+          contentContainerStyle={styles.flatList}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <Slide item={item} />}
+        />
+        <Footer />
+      </View>
     </SafeAreaView>
   );
 };
@@ -86,6 +177,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
+  },
+  listContainer: {
+    position: 'relative',
+  },
+  flatList: {
+    flexGrow: 1,
   },
 });
 
